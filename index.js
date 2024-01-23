2db1bba4a7bd054b933de1a8cc447008b640bbfd
Casey <caseychung@ufl.edu>
2024-01-22 21:32:56 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function greet() {
+    var name = window.prompt("What is your name?");
+    // user returns a string - display a message using the alert() method
+    alert("Hello, " + name);
+
+    // convert the given age from a string to a number
+    var age = window.prompt("How old are you?");
+    console.log(parseInt(age));
+
+    // confirm method
+}
